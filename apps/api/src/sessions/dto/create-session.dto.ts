96294908330c3a7505f8dd5b92c4e2c1595fdd95
nethangabrielb <bagasbas.nethangabriel@gmail.com>
2026-07-04 12:08:12 +0800

    feat: add class-transformer and class-validator dependencies; implement CreateSessionDto with validation

--- apps/api/src/sessions/dto/create-session.dto.ts
+++ apps/api/src/sessions/dto/create-session.dto.ts
@@ -1,1 +1,71 @@
-export class CreateSessionDto {}
+import {
+  IsString,
+  IsInt,
+  IsDate,
+  IsBoolean,
+  IsArray,
+  IsEnum,
+  IsMongoId,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+// session type
+export enum SessionType {
+  PROJECT = 'Projects',
+  ARTICLE = 'Article Reading',
+  DSA = 'DSA Problems',
+  STUDY = 'Study',
+}
+
+// session linked to kind
+export enum LinkedToKind {
+  PROJECT = 'Projects',
+  ARTICLE = 'Article Reading',
+  DSA = 'DSA Problems',
+  STUDY = 'Study',
+}
+
+// DTO shape of Session Todo
+class TodoDto {
+  @IsString()
+  name!: string;
+
+  @IsBoolean()
+  completed!: boolean;
+}
+
+// DTO shape of Session LinkedTo
+class LinkedToDto {
+  @IsEnum(LinkedToKind)
+  kind!: LinkedToKind;
+
+  @IsMongoId()
+  id!: string;
+}
+
+// Session overall shape
+export class CreateSessionDto {
+  @IsEnum(SessionType)
+  type!: SessionType;
+
+  @IsInt()
+  duration!: number;
+
+  @IsDate()
+  @Type(() => Date)
+  startedAt!: Date;
+
+  @IsDate()
+  @Type(() => Date)
+  endedAt!: Date;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => TodoDto)
+  todos!: Array<TodoDto>;
+
+  @ValidateNested()
+  @Type(() => LinkedToDto)
+  linkedTo!: LinkedToDto;
+}
