7bb85507eedc2133a91a3d8ae6460509ae407131
nethangabrielb <bagasbas.nethangabriel@gmail.com>
2026-07-04 11:20:44 +0800

    feat: initialize sessions module with controller, service, and DTOs

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { SessionsModule } from './sessions/sessions.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGODB_URI!),
+    SessionsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
