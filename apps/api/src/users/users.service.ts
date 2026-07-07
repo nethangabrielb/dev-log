a68e11f5a6efa857c610dd2f1fdf1a8ee3d5422d
nethangabrielb <bagasbas.nethangabriel@gmail.com>
2026-07-07 10:27:06 +0800

    feat: implement CreateUserDto and update UsersService for user creation, retrieval by username,  and email

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './schemas/users.schema';
+import { CreateUserDto } from './dto/users.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    return this.userModel.create(createUserDto);
+  }
+
+  async findByUsername(username: string) {
+    return this.userModel.findOne({ username }).exec();
+  }
+
+  async findByEmail(email: string) {
+    return this.userModel.findOne({ email }).exec();
+  }
+}
